647d047aeb370b53e73c13caa2b9ff16f01e133f
Jean Lauliac <jean@lauliac.com>
2017-06-07 20:58:00 +0100

    try to use node 7 on travis

--- tools/lib/cli.js
+++ tools/lib/cli.js
@@ -7,8 +7,8 @@ function cli(callback) {
     reporting.fatalError(1, 'cannot run as root');
     return;
   }
-  if (parseInt(process.version.substr(1).split('.')[0]) !== 6) {
-    reporting.fatalError(1, 'requires node.js v6');
+  if (parseInt(process.version.substr(1).split('.')[0]) !== 7) {
+    reporting.fatalError(1, 'requires node.js v7');
     return;
   }
   callback();
