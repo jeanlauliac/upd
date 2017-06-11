621bdfec690f3fc3bd663fac8c672e8d9fb5b748
Jean Lauliac <jean@lauliac.com>
2017-06-11 20:09:09 +0100

    fix gen_upd

--- bootstrap/gen_upd.sh
+++ bootstrap/gen_upd.sh
@@ -6,18 +6,20 @@ mkdir -p .build_files/src/lib
 clang++ -c -o .build_files/src/lib/xxhash64.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/xxhash64.cpp
 clang++ -c -o .build_files/src/lib/update_plan.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/update_plan.cpp
 clang++ -c -o .build_files/src/lib/update.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/update.cpp
-clang++ -c -o .build_files/src/lib/glob_test.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/glob_test.cpp
-clang++ -c -o .build_files/src/lib/substitution.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/substitution.cpp
 clang++ -c -o .build_files/src/lib/path_glob.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/path_glob.cpp
 clang++ -c -o .build_files/src/lib/io.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/io.cpp
-clang++ -c -o .build_files/src/lib/path.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/path.cpp
-clang++ -c -o .build_files/src/lib/inspect.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/inspect.cpp
 clang++ -c -o .build_files/src/lib/glob.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/glob.cpp
+clang++ -c -o .build_files/src/lib/manifest.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/manifest.cpp
+clang++ -c -o .build_files/src/lib/update_log.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/update_log.cpp
+clang++ -c -o .build_files/src/lib/command_line_template.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/command_line_template.cpp
 clang++ -c -o .build_files/src/lib/gen_update_map.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/gen_update_map.cpp
 clang++ -c -o .build_files/src/lib/cli.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/cli.cpp
 clang++ -c -o .build_files/src/lib/depfile.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/depfile.cpp
-clang++ -c -o .build_files/src/lib/update_log.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/update_log.cpp
-clang++ -c -o .build_files/src/lib/command_line_template.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/command_line_template.cpp
+clang++ -c -o .build_files/src/lib/path.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/path.cpp
+clang++ -c -o .build_files/src/lib/inspect.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/inspect.cpp
+clang++ -c -o .build_files/src/lib/execute_manifest.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/execute_manifest.cpp
+clang++ -c -o .build_files/src/lib/substitution.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/substitution.cpp
+clang++ -c -o .build_files/src/lib/glob_test.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/lib/glob_test.cpp
 clang++ -c -o .build_files/src/lib/xxhash.o -x c -g -Wall -fcolor-diagnostics -MMD -MF /dev/null src/lib/xxhash.c
 mkdir -p .build_files
 tools/gen_package_info.js .build_files/package.cpp /dev/null package.json
@@ -25,4 +27,4 @@ mkdir -p .build_files/src
 clang++ -c -o .build_files/src/main.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null src/main.cpp
 clang++ -c -o .build_files/package.o -std=c++14 -g -Wall -fcolor-diagnostics -stdlib=libc++ -MMD -MF /dev/null .build_files/package.cpp
 mkdir -p dist
-clang++ -o dist/upd -Wall -g -fcolor-diagnostics -stdlib=libc++ -std=c++14 .build_files/src/lib/xxhash64.o .build_files/src/lib/update_plan.o .build_files/src/lib/update.o .build_files/src/lib/glob_test.o .build_files/src/lib/substitution.o .build_files/src/lib/path_glob.o .build_files/src/lib/io.o .build_files/src/lib/path.o .build_files/src/lib/inspect.o .build_files/src/lib/glob.o .build_files/src/lib/gen_update_map.o .build_files/src/lib/cli.o .build_files/src/lib/depfile.o .build_files/src/lib/update_log.o .build_files/src/lib/command_line_template.o .build_files/src/lib/xxhash.o .build_files/package.o .build_files/src/main.o -lpthread
+clang++ -o dist/upd -Wall -g -fcolor-diagnostics -stdlib=libc++ -std=c++14 .build_files/src/lib/xxhash64.o .build_files/src/lib/update_plan.o .build_files/src/lib/update.o .build_files/src/lib/path_glob.o .build_files/src/lib/io.o .build_files/src/lib/glob.o .build_files/src/lib/manifest.o .build_files/src/lib/update_log.o .build_files/src/lib/command_line_template.o .build_files/src/lib/gen_update_map.o .build_files/src/lib/cli.o .build_files/src/lib/depfile.o .build_files/src/lib/path.o .build_files/src/lib/inspect.o .build_files/src/lib/execute_manifest.o .build_files/src/lib/substitution.o .build_files/src/lib/glob_test.o .build_files/src/lib/xxhash.o .build_files/package.o .build_files/src/main.o -lpthread
