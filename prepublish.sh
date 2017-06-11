108d131155028bdd47c92ea0e5677dc49ed5ee13
Jean Lauliac <jean@lauliac.com>
2017-06-11 10:39:51 +0100

    prepublish

--- prepublish.sh
+++ prepublish.sh
@@ -4,6 +4,3 @@ set -ev
 
 ./configure.js --compiler clang++
 dist/upd dist/upd --shell-script > bootstrap/gen_upd.sh
-
-./configure.js --compiler clang++-3.9
-dist/upd dist/upd --shell-script > bootstrap/gen_upd_for_travis.sh
