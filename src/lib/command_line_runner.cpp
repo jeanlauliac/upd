09c92f3c499b4cb5925b5eb9e46e9e0b855efb0c
Jean Lauliac <jean@lauliac.com>
2017-10-10 00:19:34 +0100

    switch to posix_spawn()

--- src/lib/command_line_runner.cpp
+++ src/lib/command_line_runner.cpp
@@ -1,16 +1,18 @@
 #include "command_line_runner.h"
 #include <iostream>
+#include <spawn.h>
 #include <stdexcept>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
+extern char **environ;
+
 namespace upd {
 
 /**
- * `fork()`/`exec()` to run a command line. We should switch to `posix_spawn()`
- * (faster on macOS).
+ * `posix_spawn()` to run a command line.
  */
 void command_line_runner::run(
   const std::string& root_path,
@@ -23,17 +25,32 @@ void command_line_runner::run(
     argv.push_back(const_cast<char*>(arg.c_str()));
   }
   argv.push_back(nullptr);
-  pid_t child_pid = fork();
-  if (child_pid == 0) {
-    close(depfile_fds[0]);
-    execvp(target.binary_path.c_str(), argv.data());
-    std::cerr << "upd: *** execvp() failed in child process" << std::endl;
-    _exit(127);
+
+  posix_spawn_file_actions_t actions;
+  if (posix_spawn_file_actions_init(&actions) != 0) {
+    throw std::runtime_error("action init failed");
   }
-  if (child_pid < 0) {
+  if (posix_spawn_file_actions_addclose(&actions, depfile_fds[0]) != 0) {
+    throw std::runtime_error("action addclose failed");
+  };
+
+  pid_t child_pid;
+  int res = posix_spawnp(
+    &child_pid,
+    target.binary_path.c_str(),
+    &actions,
+    nullptr,
+    argv.data(),
+    environ
+  );
+  if (res != 0) {
     throw std::runtime_error("command line failed");
   }
+  if (posix_spawn_file_actions_destroy(&actions) != 0) {
+    throw std::runtime_error("action destroy failed");
+  };
   close(depfile_fds[1]);
+
   int status;
   if (waitpid(child_pid, &status, 0) != child_pid) {
     throw std::runtime_error("waitpid failed");
