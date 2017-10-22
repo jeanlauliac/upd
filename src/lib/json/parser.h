085a08c2d58873a8f348fbb2b2f7f7b3841d00a3
Jean Lauliac <jean@lauliac.com>
2017-10-22 17:52:24 +0900

    parse array cleanup

--- src/lib/json/parser.h
+++ src/lib/json/parser.h
@@ -148,75 +148,6 @@ private:
 template <typename Lexer>
 struct array_reader;
 
-template <typename Lexer, typename ItemHandler>
-struct read_array_first_item_handler {
-  typedef bool return_type;
-  typedef typename ItemHandler::return_type item_type;
-
-  read_array_first_item_handler(
-    Lexer& lexer,
-    ItemHandler& item_handler,
-    item_type& value
-  ):
-    lexer_(lexer),
-    item_handler_(item_handler),
-    value_(value) {}
-
-  bool end() const { throw unexpected_end_error(); }
-
-  bool punctuation(punctuation_type type) const {
-    if (type == punctuation_type::brace_open) {
-      object_reader<Lexer> reader(lexer_);
-      value_ = item_handler_.object(reader);
-      return true;
-    }
-    if (type == punctuation_type::bracket_open) {
-      array_reader<Lexer> reader(lexer_);
-      value_ = item_handler_.array(reader);
-      return true;
-    }
-    if (type == punctuation_type::bracket_close) {
-      return false;
-    }
-    throw unexpected_punctuation_error();
-  }
-
-  bool string_literal(const std::string& literal) const {
-    value_ = item_handler_.string_literal(literal);
-    return true;
-  }
-
-  bool number_literal(float literal) const {
-    value_ = item_handler_.number_literal(literal);
-    return true;
-  }
-
-private:
-  Lexer& lexer_;
-  ItemHandler& item_handler_;
-  item_type& value_;
-};
-
-struct array_post_item_handler {
-  typedef bool return_type;
-
-  bool end() const { throw unexpected_end_error(); }
-
-  bool punctuation(punctuation_type type) const {
-    if (type == punctuation_type::comma) return true;
-    if (type == punctuation_type::bracket_close) return false;
-    throw unexpected_punctuation_error();
-  }
-
-  bool string_literal(const std::string& literal) const {
-    throw unexpected_string_error();
-  }
-
-  bool number_literal(float literal) const {
-    throw unexpected_number_error();
-  }
-};
-
 template <typename Lexer>
 struct array_reader {
   typedef Lexer lexer_type;
@@ -239,22 +170,91 @@ struct array_reader {
   ) {
     if (state_ == state_t::done) return false;
     if (state_ == state_t::init) {
-      typedef read_array_first_item_handler<Lexer, ItemHandler>
-        first_item_handler;
-      if (lexer_.next(first_item_handler(lexer_, item_handler, value))) {
+      typedef first_item_handler<ItemHandler> handler;
+      if (lexer_.next(handler(lexer_, item_handler, value))) {
         state_ = state_t::reading;
         return true;
       };
       state_ = state_t::done;
       return false;
     }
-    if (!lexer_.next(array_post_item_handler())) {
+    if (!lexer_.next(post_item_handler())) {
       state_ = state_t::done;
       return false;
     }
     value = parse_expression(lexer_, item_handler);
     return true;
   }
+
+private:
+  template <typename ItemHandler>
+  struct first_item_handler {
+    typedef bool return_type;
+    typedef typename ItemHandler::return_type item_type;
+
+    first_item_handler(
+      Lexer& lexer,
+      ItemHandler& item_handler,
+      item_type& value
+    ):
+      lexer_(lexer),
+      item_handler_(item_handler),
+      value_(value) {}
+
+    bool end() const { throw unexpected_end_error(); }
+
+    bool punctuation(punctuation_type type) const {
+      if (type == punctuation_type::brace_open) {
+        object_reader<Lexer> reader(lexer_);
+        value_ = item_handler_.object(reader);
+        return true;
+      }
+      if (type == punctuation_type::bracket_open) {
+        array_reader<Lexer> reader(lexer_);
+        value_ = item_handler_.array(reader);
+        return true;
+      }
+      if (type == punctuation_type::bracket_close) {
+        return false;
+      }
+      throw unexpected_punctuation_error();
+    }
+
+    bool string_literal(const std::string& literal) const {
+      value_ = item_handler_.string_literal(literal);
+      return true;
+    }
+
+    bool number_literal(float literal) const {
+      value_ = item_handler_.number_literal(literal);
+      return true;
+    }
+
+  private:
+    Lexer& lexer_;
+    ItemHandler& item_handler_;
+    item_type& value_;
+  };
+
+  struct post_item_handler {
+    typedef bool return_type;
+
+    bool end() const { throw unexpected_end_error(); }
+
+    bool punctuation(punctuation_type type) const {
+      if (type == punctuation_type::comma) return true;
+      if (type == punctuation_type::bracket_close) return false;
+      throw unexpected_punctuation_error();
+    }
+
+    bool string_literal(const std::string& literal) const {
+      throw unexpected_string_error();
+    }
+
+    bool number_literal(float literal) const {
+      throw unexpected_number_error();
+    }
+  };
 
 private:
   Lexer& lexer_;
