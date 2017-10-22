41ff09131243d8958452a75aad8f0196c22acd7b
Jean Lauliac <jean@lauliac.com>
2017-10-22 14:22:04 +0900

    comment

--- tools/lib/writeNodeDepFile.js
+++ tools/lib/writeNodeDepFile.js
@@ -7,7 +7,11 @@ const fs = require('fs');
 
 /**
  * Writes a depfile that describe all the Node.js modules currently loaded as
- * dependencies of the specified target file.
+ * dependencies of the specified target file. For example if the current script
+ * is "foo.js" and requires "bar.js" itself, it could write something like:
+ *
+ *     target.txt: foo.js bar.js
+ *
  */
 function writeNodeDepFile(depFilePath: string, targetFilePath: string) {
   writeDepFile(fs.createWriteStream(depFilePath), [{
