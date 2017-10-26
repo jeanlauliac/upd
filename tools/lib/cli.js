08651967daeb162e0725ce98eef55b4b4ea99a5d
Jean Lauliac <jean@lauliac.com>
2017-10-26 19:27:36 +0100

    upgrade to Node v8

--- tools/lib/cli.js
+++ tools/lib/cli.js
@@ -13,8 +13,8 @@ function cli(callback: () => mixed) {
     reporting.fatalError(1, 'cannot run as root');
     return;
   }
-  if (parseInt(process.version.substr(1).split('.')[0]) !== 7) {
-    reporting.fatalError(1, 'requires node.js v7');
+  if (parseInt(process.version.substr(1).split('.')[0]) !== 8) {
+    reporting.fatalError(1, 'requires node.js v8');
     return;
   }
   callback();
