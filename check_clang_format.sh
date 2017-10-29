c8eb5aaef57e57b10fef095d83672ae293eb5862
Jean Lauliac <jean@lauliac.com>
2017-10-29 23:10:24 +0000

    fix find command for CI

--- check_clang_format.sh
+++ check_clang_format.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-files=$(find -E src -regex '.*\.(cpp|h|cppt)')
+files=$(find src -name *.cpp -or -name *.h -or -name *.cppt)
 diff <(cat $files) <(node_modules/.bin/clang-format $files)
 if [ $? -ne 0 ]; then
   echo 'error: code not properly formatted; run `yarn format`'
