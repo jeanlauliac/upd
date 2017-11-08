491236c7d475a14a78406eeb32423679b408dd4a
Jean Lauliac <jean@lauliac.com>
2017-11-08 22:08:19 +0000

    install libstdc++6 for travis

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ set -ev
 
 ./check_clang_format.sh
 node_modules/.bin/flow
+bootstrap/upd_linux update gen/tools/lib/__tests__/writeDepFile-test.js
 node_modules/.bin/jest
 bootstrap/upd_linux update dist/unit_tests
 dist/unit_tests | node_modules/.bin/faucet
