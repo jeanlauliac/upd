0e83d45bafb9349aa036287e31383cb368a0ddf2
Jean Lauliac <jean@lauliac.com>
2017-11-13 22:54:21 +0000

    fix preinstall and tests

---
+++ preinstall.sh
@@ -0,0 +1,11 @@
+#/bin/sh
+
+set -e
+
+rm -f bootstrap/upd
+
+if [[ "$(uname)" == "Darwin" ]]; then
+  ln -s upd_darwin bootstrap/upd
+else
+  ln -s upd_linux bootstrap/upd
+fi
--- test.sh
+++ test.sh
@@ -2,10 +2,9 @@
 
 set -ev
 
-./check_clang_format.sh
 node_modules/.bin/flow
-bootstrap/upd_linux update gen/tools/lib/__tests__/writeDepFile-test.js
+bootstrap/upd update --all
 node_modules/.bin/jest
-bootstrap/upd_linux update dist/unit_tests
 dist/unit_tests | node_modules/.bin/faucet
 e2e_tests/run.js
+./check_clang_format.sh
