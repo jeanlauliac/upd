84dc1c3bbd01b700450a7a8eb5e6f182601c7d89
Jean Lauliac <jean@lauliac.com>
2017-11-13 23:00:44 +0000

    no more prepublish script

--- prepublish.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-
-set -ev
-
-./configure.js --compiler clang++
-dist/upd script dist/upd > bootstrap/gen_upd.sh
-
-./configure.js --compiler g++
-dist/upd script dist/upd > bootstrap/gen_upd_g++.sh
