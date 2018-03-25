a124fa0181f8db92b62fe3173ce3be918c0afb2f
Jean Lauliac <jeanlauliac@fb.com>
2018-03-25 11:53:44 +0200

    switch update_rule to autogen

--- src/manifest/manifest.h
+++ src/manifest/manifest.h
@@ -8,30 +8,6 @@
 namespace upd {
 namespace manifest {
 
-struct update_rule {
-  size_t command_line_ix;
-  std::vector<update_rule_input> inputs;
-  std::vector<update_rule_input> order_only_dependencies;
-  substitution::pattern output;
-};
-
-inline bool operator==(const update_rule &left, const update_rule &right) {
-  return left.command_line_ix == right.command_line_ix &&
-         left.inputs == right.inputs &&
-         left.order_only_dependencies == right.order_only_dependencies &&
-         left.output == right.output;
-}
-
-inline std::string inspect(const update_rule &value,
-                           const inspect_options &options) {
-  collection_inspector insp("upd::manifest::update_rule", options);
-  insp.push_back("command_line_ix", value.command_line_ix);
-  insp.push_back("inputs", value.inputs);
-  insp.push_back("order_only_dependencies", value.order_only_dependencies);
-  insp.push_back("output", value.output);
-  return insp.result();
-}
-
 struct manifest {
   std::vector<command_line_template> command_line_templates;
   std::vector<path_glob::pattern> source_patterns;
